750ff47ff815478e2117be30f9ac9790324e6283
alek101 <47416711+alek101@users.noreply.github.com>
2021-01-22 08:23:29 +0100

    2

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Citrus Test</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="wrapper">
-        <div class="item"></div>
+        <div class="item">p</div>
         <div class="item"></div>
         <div class="item"></div>
         <div class="item"></div>
